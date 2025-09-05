2aa101cb5157926f8cef072e021ba1a25218485a
minwoo129 <key0129mw@naver.com>
2025-09-05 19:29:29 +0900

    feat: #6 예시 프로젝트 기능 연결

--- examples/react-example/src/main.tsx
+++ examples/react-example/src/main.tsx
@@ -1,10 +1,17 @@
+import { ContextMenuProvider, FEToolkitProvider } from '@fetoolkit/react/src';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
+import AppContextMenu from './components/AppContextMenu.tsx';
+import { Validators } from './constants/validation.ts';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FEToolkitProvider validators={Validators}>
+      <ContextMenuProvider menuComponent={<AppContextMenu />}>
+        <App />
+      </ContextMenuProvider>
+    </FEToolkitProvider>
   </StrictMode>,
 );
