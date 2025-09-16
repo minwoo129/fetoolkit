76e5f07bf5a39100e70cd9c1d8cc86993cea86c8
minwoo129 <key0129mw@naver.com>
2025-09-16 12:20:57 +0900

    feat: #51 테스트 스크립트 코드 수정

--- apps/react-test/__tests__/render.spec.ts
+++ apps/react-test/__tests__/render.spec.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-  await expect(page).toHaveTitle(/Hello World/);
+  await expect(page.getByText('Hello World')).toBeVisible();
 });
