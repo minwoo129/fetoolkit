d741ced11d2669781af19fa7732ad4b4064c0bfc
minwoo129 <key0129mw@naver.com>
2025-09-16 12:10:31 +0900

    feat: #51 playwright 환경 설정 및 E2E 테스트 파이프라인 추가

--- apps/react-test/src/main.tsx
+++ apps/react-test/src/main.tsx
@@ -1,3 +1,4 @@
+import { FEToolkitProvider } from '@fetoolkit/react';
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
@@ -5,6 +6,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FEToolkitProvider>
+      <App />
+    </FEToolkitProvider>
   </StrictMode>,
 );
