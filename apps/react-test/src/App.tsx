2b075759ab9ca843ad7f61248fcabe019aba48e1
minwoo129 <key0129mw@naver.com>
2025-09-16 12:27:05 +0900

    feat: #51 errorboundary e2e 테스트 스크립트 추가

--- apps/react-test/src/App.tsx
+++ apps/react-test/src/App.tsx
@@ -1,9 +1,18 @@
+import { ErrorBoundary } from '@fetoolkit/react';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ErrorBoundaryTestPage from './pages/ErrorBoundaryTestPage';
+import MainPage from './pages/MainPage';
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ErrorBoundary renderFallback={() => <div>Error Test</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/error-test" element={<ErrorBoundaryTestPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 };
 
