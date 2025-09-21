9b42f57ab662ae8e689c457d56f242fe43a1f908
minwoo129 <key0129mw@naver.com>
2025-09-21 16:29:41 +0900

    feat: #74 admin-react 프로젝트 생성

---
+++ packages/admin-react/README.md
@@ -0,0 +1,11 @@
+# `admin-react`
+
+> TODO: description
+
+## Usage
+
+```
+const adminReact = require('admin-react');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/admin-react/vite.config.ts
@@ -0,0 +1,43 @@
+/// <reference types="vite/client" />
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      include: ['src/**/*', 'index.ts'],
+      tsconfigPath: 'tsconfig.app.json',
+    }),
+    react(),
+  ],
+  build: {
+    lib: {
+      entry: {
+        index: path.resolve(__dirname, 'index.ts'),
+      },
+      name: '@fetoolkit/admin-react',
+      fileName: 'index',
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        interop: 'auto',
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+    cssCodeSplit: false, // CSS를 하나의 파일로 번들링
+  },
+  esbuild: {
+    jsx: 'automatic',
+  },
+});
