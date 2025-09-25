8b68e5303fed9bcf2714c6954c573e20cc08855e
minwoo129 <key0129mw@naver.com>
2025-09-25 12:27:47 +0900

    feat: #74 MenuTemplate 컴포넌트 재개발 임시저장

--- packages/admin-react/vite.config.ts
+++ packages/admin-react/vite.config.ts
@@ -1,8 +1,9 @@
 /// <reference types="vite/client" />
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import libcss from 'vite-plugin-libcss';
 
 export default defineConfig({
   plugins: [
@@ -11,9 +12,8 @@ export default defineConfig({
       include: ['src/**/*', 'index.ts'],
       tsconfigPath: 'tsconfig.app.json',
     }),
-    react({
-      plugins: [['@swc/plugin-emotion', {}]],
-    }),
+    react(),
+    libcss(),
   ],
   build: {
     lib: {
@@ -25,13 +25,12 @@ export default defineConfig({
       formats: ['es', 'umd'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom', '@emotion/react', '@emotion/styled'],
+      external: ['react', 'react-dom', 'react-router-dom'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
-          '@emotion/react': 'EmotionReact',
-          '@emotion/styled': 'EmotionStyled',
+          'react-router-dom': 'ReactRouterDOM',
         },
         interop: 'auto',
       },
@@ -39,7 +38,6 @@ export default defineConfig({
     commonjsOptions: {
       esmExternals: ['react'],
     },
-    cssCodeSplit: false, // CSS를 하나의 파일로 번들링
   },
   esbuild: {
     jsx: 'automatic',
