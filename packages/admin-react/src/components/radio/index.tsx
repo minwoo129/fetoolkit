fc494d51642c30335c64f84e044e581ec259771d
minwoo129 <key0129mw@naver.com>
2025-10-13 14:37:55 +0900

    feat: #74 radio 컴포넌트 testid props 추가

--- packages/admin-react/src/components/radio/index.tsx
+++ packages/admin-react/src/components/radio/index.tsx
@@ -3,7 +3,10 @@ import classNames from 'classnames';
 import React from 'react';
 import '../../css/radio.css';
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+type InputProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  'data-testid'
+>;
 
 type AdminRadioSize = 'sm' | 'md' | 'lg' | 'xl';
 
@@ -17,18 +20,21 @@ type AdminRadioColor =
 interface Props extends Omit<InputProps, 'size'> {
   size?: AdminRadioSize;
   color?: AdminRadioColor;
+  dataTestId?: string;
 }
 
 export const AdminRadio = ({
   size = 'md',
   color = 'primary',
   className,
+  dataTestId,
   ...props
 }: Props) => {
   return (
     <input
       type="radio"
       className={classNames('radio', size, color, className)}
+      data-testid={dataTestId}
       {...props}
     />
   );
