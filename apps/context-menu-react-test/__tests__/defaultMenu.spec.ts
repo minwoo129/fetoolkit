0e51e63f02571a9268fcc75eba90cea34cb8be5f
minwoo129 <key0129mw@naver.com>
2025-10-17 14:10:16 +0900

    feat: #94 디폴트 ContextMenu E2E 테스트 로직 추가

---
+++ apps/context-menu-react-test/__tests__/defaultMenu.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@playwright/test';
+
+const URL = 'http://localhost:3003/test-page-1';
+
+test('렌더링 테스트', async ({ page }) => {
+  await page.goto(URL);
+  await page.getByText('Test Element').click({
+    button: 'right',
+  });
+
+  await expect(page.getByTestId('page-layout-default-test')).toBeVisible();
+});
+
+test('버튼 클릭 테스트', async ({ page }) => {
+  await page.goto('http://localhost:3003/test-page-1');
+  await page.getByText('Test Element').click({
+    button: 'right',
+  });
+  await page.getByRole('listitem').filter({ hasText: '햇빛' }).click();
+  await expect(page.getByTestId('print-value')).toHaveText('test-menu-1-sun');
+});
--- apps/context-menu-react-test/src/pages/ContextMenuTestPage1.tsx
+++ apps/context-menu-react-test/src/pages/ContextMenuTestPage1.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import { useContextMenu } from '@fetoolkit/context-menu-react';
+import React, { useEffect, useState } from 'react';
 import '../css/testPage1.css';
 
 const ContextMenuTestPage1 = () => {
+  const [printValue, setPrintValue] = useState('');
+  const { onClickedContextMenuItem } = useContextMenu();
+  useEffect(() => {
+    const unsubscribe = onClickedContextMenuItem((data) => {
+      if (data) {
+        setPrintValue(`${data.menuId}-${data.value}`);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [onClickedContextMenuItem]);
   return (
     <div className="mainLayer">
-      <div className="row-items-grid"></div>
+      <div className="row-items-grid">
+        <div className="test-element" context-menu-id="test-menu-1">
+          Test Element
+        </div>
+      </div>
+      <div className="row-items-grid">
+        <h3 data-testid="print-value">{printValue}</h3>
+      </div>
     </div>
   );
 };
