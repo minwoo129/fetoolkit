48ea7e12d196db14f65ade980179c84c7c8a1057
minwoo129 <key0129mw@naver.com>
2025-10-17 13:38:34 +0900

    feat: #94 테스트용 페이지 추가

--- apps/context-menu-react-test/src/App.tsx
+++ apps/context-menu-react-test/src/App.tsx
@@ -1,5 +1,7 @@
+import { withContextMenu } from '@fetoolkit/context-menu-react';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ContextMenuTestPage1 from './pages/ContextMenuTestPage1';
 import Main from './pages/Main';
 
 const App = () => {
@@ -7,6 +9,10 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route
+          path="/test-page-1"
+          element={withContextMenu(<ContextMenuTestPage1 />)}
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ apps/context-menu-react-test/src/pages/ContextMenuTestPage1.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/testPage1.css';
+
+const ContextMenuTestPage1 = () => {
+  return (
+    <div className="mainLayer">
+      <div className="row-items-grid"></div>
+    </div>
+  );
+};
+
+export default ContextMenuTestPage1;
