e5c23f5c65d3b5d4293883ca0c8257f08659b32e
minwoo129 <key0129mw@naver.com>
2025-10-22 13:17:12 +0900

    feat: #98 E2E 테스트 스크립트 수정

--- apps/user-agent-test/__tests__/useragent.spec.ts
+++ apps/user-agent-test/__tests__/useragent.spec.ts
@@ -4,6 +4,5 @@ test('useragent', async ({ page }) => {
   await page.goto('http://localhost:3004');
   const testText = await page.getByTestId('user-agent-data').textContent();
 
-  console.log('testText: ', testText);
-  expect(1).toBe(1);
+  expect(testText).toBeDefined();
 });
