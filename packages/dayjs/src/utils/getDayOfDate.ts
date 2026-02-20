d8c8c27696e59de87a0aa3e0cb18747eb6c83235
minwoo129 <key0129mw@naver.com>
2026-02-20 19:22:47 +0900

    feat: #175 요일 정보 반환 함수 로직 수정
    - 제미나이 코드리뷰 내용 반영

--- packages/dayjs/src/utils/getDayOfDate.ts
+++ packages/dayjs/src/utils/getDayOfDate.ts
@@ -1,54 +1,23 @@
 import dayjs from 'dayjs';
 import type { DayType } from '../types/dayType';
+
+const DAYS_KOR = [
+  '일요일',
+  '월요일',
+  '화요일',
+  '수요일',
+  '목요일',
+  '금요일',
+  '토요일',
+] as const;
+const DAYS_ENG = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'] as const;
+
 export const getDayOfDate = (date: dayjs.Dayjs): DayType => {
   const day = date.day();
 
-  if (day === 0) {
-    return {
-      idx: 0,
-      dayKor: '일요일',
-      dayEng: 'SUN',
-    };
-  }
-  if (day === 1) {
-    return {
-      idx: 1,
-      dayKor: '월요일',
-      dayEng: 'MON',
-    };
-  }
-  if (day === 2) {
-    return {
-      idx: 2,
-      dayKor: '화요일',
-      dayEng: 'TUE',
-    };
-  }
-  if (day === 3) {
-    return {
-      idx: 3,
-      dayKor: '수요일',
-      dayEng: 'WED',
-    };
-  }
-  if (day === 4) {
-    return {
-      idx: 4,
-      dayKor: '목요일',
-      dayEng: 'THU',
-    };
-  }
-  if (day === 5) {
-    return {
-      idx: 5,
-      dayKor: '금요일',
-      dayEng: 'FRI',
-    };
-  }
-
   return {
-    idx: 6,
-    dayKor: '토요일',
-    dayEng: 'SAT',
+    idx: day,
+    dayKor: DAYS_KOR[day],
+    dayEng: DAYS_ENG[day],
   };
 };
